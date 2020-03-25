a3883957ff7acc9e6b52bb8a4efa6bcb31bc9044
eeki <ifirtree@gmail.com>
2020-03-25 12:29:59 +0300

    design fixes

--- src/js/utils/forumDesignUtils.js
+++ src/js/utils/forumDesignUtils.js
@@ -7,13 +7,22 @@ export function getBackgroundImage() {
 
 	let backgroundImage;
 
-	if (hours > 6 && hours < 20) {
+	if (hours > 5 && hours < 8) {
+		backgroundImage = "url('/images/kola_sunrise.jpg')";
+	}
+	if (hours > 8 && hours < 11) {
+		backgroundImage = "url('/images/kola_morning.jpg')";
+	}
+	else if (hours > 11 && hours < 18) {
 		backgroundImage = "url('/images/kola_day.jpg')";
 	}
-	else if (hours > 20 && hours < 1) {
+	else if (hours > 18 && hours < 21) {
+		backgroundImage = "url('/images/kola_sunset.jpg')";
+	}
+	else if (hours > 21 && hours < 1) {
 		backgroundImage = "url('/images/kola_starlight_night.jpg')";
 	}
-	else {
+	else if (hours > 1 && hours < 5) {
 		backgroundImage = "url('/images/kola_northern_lights.jpg')";
 	}
 
